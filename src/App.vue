53a594cc35076691f3798a1d4b19ccaa76f53ca4
Ivan Zhdanov <tain.krios@gmail.com>
2022-03-01 23:33:05 +0300

    result component, style fix

--- src/App.vue
+++ src/App.vue
@@ -6,33 +6,41 @@
     :delay="state.delay" 
     @end="endGame"
   />
-  <p>Reaction time: {{ state.score }}</p>
+  <Results 
+    v-if="state.showResults"
+    :score="state.score"
+  />
 </template>
 
 <script setup lang="ts">
 import { reactive } from 'vue'
 import Block from '@/components/Block.vue'
+import Results from '@/components/Results.vue'
 
 interface State {
   isPlaying: boolean
   delay: number | null
   score: number | null
+  showResults: boolean
 }
 
 const state = reactive<State>({
   isPlaying: false,
   delay: null,
-  score: null
+  score: null,
+  showResults: false
 })
 
 function start () {
   state.isPlaying = true
   state.delay = 2000 + Math.random() * 5000
+  state.showResults = false
 }
 
 function endGame (reactionTime: number) {
   state.score = reactionTime
   state.isPlaying = false
+  state.showResults = true
 }
 
 </script>
@@ -45,5 +53,21 @@ function endGame (reactionTime: number) {
   text-align: center;
   color: #444;
   margin-top: 60px;
+}
+
+button {
+  background: #739be6;
+  color: white;
+  border: none;
+  padding: 8px 16px;
+  border-radius: 4px;
+  font-size: 16px;
+  letter-spacing: 1px;
+  cursor: pointer;
+  margin: 10px;
+}
+button:disabled {
+  opacity: 0.2;
+  cursor: not-allowed;
 }
 </style>
--- src/components/Results.vue
+++ src/components/Results.vue
@@ -1,13 +1,41 @@
 <template>
-  <div>
-
-  </div>
+  <p>Reaction time: {{ props.score }} </p>
+  <p class="rank">{{ state.rank }}</p>
 </template>
 
 <script setup lang="ts">
+import { onMounted, reactive } from "vue"
+
+interface Props {
+  score: number
+}
+
+const props = withDefaults(defineProps<Props>(), {
+  score: 0
+})
+
+interface State {
+  rank: string
+}
+
+const state = reactive<State>({rank: ''})
+
+onMounted(() => {
+  if (props.score < 250) {
+    state.rank = 'Ninja Fingers'
+  } else if (props.score < 400) {
+    state.rank = 'Rapid Reflexes'
+  } else {
+    state.rank = 'Snail pace...'
+  }
+})
 
 </script>
 
-<style scoped>
-
+<style lang="scss" scoped>
+.rank {
+  font-size: 1.4em;
+  color: #739be6;
+  font-weight: bold;
+}
 </style>
